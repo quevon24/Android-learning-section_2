0c723a7c49ed5f21ef8314e75a8d962d5594ed86
Kevin ramirez <kevin.ramirez@jbge.com.mx>
2017-11-24 13:29:21 -0600

    Boton optionsmenu

--- app/src/main/java/com/seccion2/seccion_02/GridActivity.java
+++ app/src/main/java/com/seccion2/seccion_02/GridActivity.java
@@ -2,10 +2,12 @@ package com.seccion2.seccion_02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,6 +17,8 @@ public class GridActivity extends AppCompatActivity {
 
     private GridView gridView;
     private List<String> names;
+    private MyAdapter myAdapter;
+    private int counter = 0;
 
 
     @Override
@@ -30,19 +34,6 @@ public class GridActivity extends AppCompatActivity {
         names.add("Fernando");
         names.add("Ruben");
         names.add("Santiago");
-        names.add("Alejandro");
-        names.add("Fernando");
-        names.add("Ruben");
-        names.add("Santiago");
-        names.add("Alejandro");
-        names.add("Fernando");
-        names.add("Ruben");
-        names.add("Santiago");
-        names.add("Alejandro");
-        names.add("Fernando");
-        names.add("Ruben");
-        names.add("Santiago");
-
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -52,9 +43,33 @@ public class GridActivity extends AppCompatActivity {
         });
 
         // Enlazamos con nuestro adaptador personalizado
-        MyAdapter myadapter = new MyAdapter(this, R.layout.grid_item, names);
-        gridView.setAdapter(myadapter);
+        myAdapter = new MyAdapter(this, R.layout.grid_item, names);
+        gridView.setAdapter(myAdapter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Agregar elemento al action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                // Añadir nuevo nombre
+                this.names.add("Added nº" + (++counter));
+                // informar al adaptador de que se ha producido un cambio
+                this.myAdapter.notifyDataSetChanged();
+                // refrescar adaptador
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
 }
